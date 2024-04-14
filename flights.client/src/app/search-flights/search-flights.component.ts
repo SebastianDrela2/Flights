a037ae9879d8598c8a7065996b700b388f9110fb
Sebastian Drela <sebastian.drela@wp.pl>
2024-04-14 20:38:48 +0200

    improve visuals, map plane data to objects

--- flights.client/src/app/search-flights/search-flights.component.ts
+++ flights.client/src/app/search-flights/search-flights.component.ts
@@ -7,18 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchFlightsComponent implements OnInit {
 
-  searchResult: any = [
-
-    "Polish AirLines",
-    "Cloudy Fliers",
-    "NineTail Birds"
-  ]
-
   title = "Search Flights";
+
+  searchResult: FlightRm[] = [
+    {
+        airline: "Polish AirLines",
+        arival: {time: Date.now().toString(), place: "Warsaw"},
+        departure: { time: Date.now().toString(), place: "Krakow" },
+        price: '600',
+        remainingNumOfSits: 500
+    },
+    {
+        airline: "Cloudy Fliers",
+        arival: { time: Date.now().toString(), place: "San Francisco" },
+        departure: { time: Date.now().toString(), place: "New York" },
+        price: '350',
+        remainingNumOfSits: 60
+    },
+    {
+        airline: "NineTail Birds",
+        arival: { time: Date.now().toString(), place: "London" },
+        departure: { time: Date.now().toString(), place: "Cambridge" },
+        price: '350',
+        remainingNumOfSits: 60
+    }
+  ]  
 
   ngOnInit(): void
   {
         
   }
 
+}
+
+export interface FlightRm
+{
+  airline: string,
+  arival: TimePlaceRm,
+  departure: TimePlaceRm,
+  price: string,
+  remainingNumOfSits: number
+}
+
+export interface TimePlaceRm
+{
+  place: string,
+  time: string
 }
